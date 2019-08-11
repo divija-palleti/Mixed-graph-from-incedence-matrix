d55350061b124a67e08b41e3afbf3572cb3665be
Divija Palleti <IHM2016005@iiita.ac.in>
2019-08-11 16:19:33 +0530

    Added marking of the vertices
    The marking of the vertices is changed according to the quadrant in which they are present.

--- ince-g.py
+++ ince-g.py
@@ -1,45 +1,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-from itertools import permutations 
+# from itertools import permutations 
 
 
-midx, midy = 50, 50
-
-radius = 25
-
-
-matrix = [[1, 1, 0, 0], [0, 0, 1, 1], [1, 0, 1, 0], [0, 1, 0, 1]]
-
-# matrix =[[1,1,1,0, 0, 0], [1, 0, 0,1,1,0],[0,1,0,1,0,1], [0,0,1,0,1,1]]
-
-matrix = [[1, 1, -1, 0,0,0,0,0], [-1,0,0,1,0,1,0,0],[0,-1,0,0,1,0,0,0], [0,0,1,-1,-1,0,1,1,], [0,0,0,0,0,-1,-1,0]]
-
-
-matrix = np.array(matrix)
-
-print(matrix.shape,"matrix shape")
-
-verts = []
-start =0
-end = 0
-no_vert = matrix.shape[0]
-no_edge = matrix.shape[1]
-for i in range(no_vert):
-    verts.append(i)
-permuts =(verts)
-print
+def whichqua(x,y):
+    print(x,y,"insode")
+    if(x>= 50 and y>=50):
+        return 0
+    if(x>= 50 and y<50):
+        return 1
+    if(x<= 50 and y<=50):
+        return 2
+    if(x< 50 and y>=50):
+        
+        return 3
+    return 4
 
 
-n = matrix.shape[0]
-pi = 3.141
-x = []
-y = []
-for i in range(n):
-    x.append(radius * math.cos(2*pi*i/n) + midx)
-    y.append(radius * math.sin(2*pi*i/n) + midy)
+def mark( x, y, val):
+    qua = whichqua(x,y)
+    distance = 1.5
+    print( x, y, qua , "printing qua")
+    if(qua == 0):
+        x = x + distance
+        y = y+distance
 
-for i in range(1):
+    if(qua == 1):
+        x = x+distance
+        y = y-distance
+
+    if(qua == 2):
+        x = x-distance -1
+        y = y-distance -1
+    if(qua == 3):
+        x = x-distance
+        y = y+distance
+    plt.text(x,y, val, fontsize=9)
+
+
+def plotgraph(edges):
+
+
+    plt.axis([0, 100, 0, 100])
+    for i in range(edges.shape[0]):
+        print(edges[i][0], edges[i][1])
+        x0 = x[edges[i][0]]
+        x1 = x[edges[i][1]]
+        y0 = y[edges[i][0]]
+        y1 = y[edges[i][1]]
+        directed = edges[i][2]
+        ax = plt.axes()
+        if(directed == 0):
+            ax.annotate("", xy=(x1,y1), xytext=(x0, y0), arrowprops=dict(arrowstyle="-"))
+            
+        else:
+            ax.annotate("", xy=(x1,y1), xytext=(x0, y0), arrowprops=dict(arrowstyle="->"))
+        
+        plt.scatter(x0, y0, marker='.', color='red')
+        plt.scatter(x1, y1, marker='.', color='red')
+
+        mark( x0, y0, edges[i][0] )
+        mark( x1, y1, edges[i][1] )
+        #to indicate the self loop
+        if( x1 == x0 and y1 == y0):
+            plt.scatter(x1, y1, marker='.', color='green')
+    
+    plt.show()
+
+
+def drawgraph(permuts, no_edge, no_vert, x, y, start, end, verts):
+
+   
     p = permuts
     print(p,"p")
     edges = []
@@ -55,8 +87,6 @@ for i in range(1):
                 flag=1
                 end = p[j]
         
-        
-        
         if(flag == 1):
             
 
@@ -65,8 +95,10 @@ for i in range(1):
             if(length == 1):
                 ones.append(ones[0])
             ones.append(1)
+
             
         else :
+
             length  = len(ones)
             if(length == 1):
                 ones.append(ones[0])
@@ -77,34 +109,51 @@ for i in range(1):
     edges = np.array(edges)
 
     print(edges,"edges")
+    return edges
     
-    plt.axis([0, 100, 0, 100])
-    for i in range(edges.shape[0]):
-        print(edges[i][0], edges[i][1])
-        x0 = x[edges[i][0]]
-        x1 = x[edges[i][1]]
-        y0 = y[edges[i][0]]
-        y1 = y[edges[i][1]]
-        directed = edges[i][2]
-        # plt.plot([x[edges[i][0]], x[edges[i][1]]], [y[edges[i][0]], y[edges[i][1]]], 'ro-')
-        ax = plt.axes()
-        # ax.arrow(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k')
-        ax = plt.axes()
-        # ax.arrow(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k')
-        if(directed == 0):
-            ax.annotate("", xy=(x1,y1), xytext=(x0, y0), arrowprops=dict(arrowstyle="-"))
-            
-        else:
-            ax.annotate("", xy=(x1,y1), xytext=(x0, y0), arrowprops=dict(arrowstyle="->"))
-        plt.scatter(x0, y0, marker='.', color='red')
-        plt.scatter(x1, y1, marker='.', color='red')
-        plt.text(x1+0.3, y1+0.3, edges[i][1], fontsize=9)
-        plt.text(x0+0.3, y0+0.3, edges[i][0], fontsize=9)
-
-        if( x1 == x0 and y1 == y0):
-            plt.scatter(x1, y1, marker='.', color='green')
-            
+    
 
 
+    
 
-    plt.show()
+
+if __name__ == "__main__":
+
+
+    midx, midy = 50, 50
+    radius = 25
+    matrix = [[1, 1, 0, 0], [0, 0, 1, 1], [1, 0, 1, 0], [0, 1, 0, 1]]
+
+    # matrix =[[1,1,1,0, 0, 0], [1, 0, 0,1,1,0],[0,1,0,1,0,1], [0,0,1,0,1,1]]
+
+    matrix = [[1, 1, 1, 0,0,0,0,0], [1,0,0,1,0,1,0,0],[0,-1,0,0,1,0,0,0], [0,0,1,-1,-1,0,1,1,], [0,0,0,0,0,-1,-1,0]]
+
+
+    matrix = np.array(matrix)
+
+    print(matrix.shape,"matrix shape")
+
+    verts = []
+    start = 0
+    end = 0
+    no_vert = matrix.shape[0]
+    no_edge = matrix.shape[1]
+    for i in range(no_vert):
+        verts.append(i)
+    permuts =(verts)
+
+
+
+    n = matrix.shape[0]
+    pi = 3.141
+    x = []
+    y = []
+    for i in range(n):
+        x.append(radius * math.cos(2*pi*i/n) + midx)
+        y.append(radius * math.sin(2*pi*i/n) + midy)
+
+    edges = drawgraph(permuts, no_edge, no_vert, x, y, start, end, verts)
+
+    plotgraph(edges)
+
+    
